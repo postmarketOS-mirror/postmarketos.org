f42fd8e1259ac1eebb11f6b61c28ebc1f2c88102
Martijn Braam <martijn@brixit.nl>
2019-05-29 00:25:37 +0200

    Dark mode fixup

--- freeze.py
+++ freeze.py
@@ -28,12 +28,4 @@ def wiki_redirect():
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dark', help='Render dark version of the website', action='store_true')
-    args = parser.parse_args()
-    if args.dark:
-        app.config['DARK'] = True
-        app.config['FREEZER_BASE_URL'] = 'https://nm.postmarketos.org/'
-
     freezer.freeze()
